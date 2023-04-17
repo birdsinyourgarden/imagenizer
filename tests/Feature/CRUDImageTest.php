c173cd4c64ed51dca3b9c59af0b66d1a641cb923
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-04-17 21:16:22 +0200

    test Create from CRUD + test Read from CRUD

--- tests/Feature/CRUDImageTest.php
+++ tests/Feature/CRUDImageTest.php
@@ -10,11 +10,33 @@ class CRUDImageTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_listImageAppearInHomeView(){
+        $this->withExceptionHandling();
+        $images = Image::factory(2)->create();
+        $image = $images[0];
+        $response = $this->get('/');
+        $response->assertSee($image->name);
+        $response->assertStatus(200)
+                ->assertViewIs('home');
+    }
+
     public function test_anImageCanBeDeleted(){
         $this->withExceptionHandling();
         $image = Image::factory()->create();
         $this->assertCount(1, Image::all());
         $response = $this->delete(route('deleteImage', $image->id));
         $this->assertCount(0, Image::all());
+    }
+
+    public function test_anImageCanBeCreated(){
+        $this->withExceptionHandling();
+        $response = $this->post((route('storeImage')),
+        [
+            'title' => 'title',
+            'description' => 'description',
+            'img' => 'img',
+            'year' => 'year',
+        ]);
+        $this->assertCount(1, Image::all());
     }
 }
