62f81d8f7450dd94a3b631d873816dc73619af61
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-04-18 22:09:15 +0200

    header + link CSS modified

---
+++ resources/views/header.blade.php
@@ -0,0 +1,5 @@
+<header class="mainHeader mb-4">
+        <div class="siteIdentity">
+            <img class="img-fluid" src="https://res.cloudinary.com/dav8u32ps/image/upload/v1681847618/siteLogo.png" alt="Imagenizer logo" >
+        </div>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <!-- CSS only -->
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <div id="app">
@@ -76,7 +76,7 @@
                 </div>
             </div>
         </nav>
-
+        @include('header')
         <main class="py-4">
             @yield('content')
         </main>
