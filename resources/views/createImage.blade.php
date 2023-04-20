631044c76598fac1ec830a92e573201ef65e8dd9
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-04-20 19:57:51 +0200

    style of createImage and editImage views

--- resources/views/createImage.blade.php
+++ resources/views/createImage.blade.php
@@ -10,18 +10,18 @@
 						@csrf
 						<div class="input-group mb-3">
 							<span class="input-group-text" id="label-title">Title</span>
-							<input type="text" class="form-control" placeholder="title" aria-label="Title" aria-describedby="label-title" maxlength="150" required>
+							<input type="text" name="title" class="form-control" placeholder="title" aria-label="title" aria-describedby="label-title" maxlength="150" required>
 						</div>
 						<div class="input-group mb-3">
 							<span class="input-group-text" id="label-year">Year</span>
-							<input type="text" class="form-control" placeholder="year" aria-labelledby="label-year" maxlength="4">
+							<input type="text" name="year" class="form-control" placeholder="year" aria-label="year" aria-labelledby="label-year" maxlength="4">
 						</div>
 						<div class="input-group mb-3">
 							<span class="input-group-text" id="label-description">Description</span>
-							<textarea name="description" rows="3" class="form-control" placeholder="description" aria-labelledby="label-description" maxlength="500"></textarea>
+							<textarea name="description" rows="3" class="form-control" placeholder="description" aria-label="description" aria-labelledby="label-description" maxlength="500"></textarea>
 						</div>
 						<div class="input-group mb-3">
-							<input type="file" name="img" class="form-control" placeholder="URL photo" maxlength="255" required>
+							<input type="file" name="img" class="form-control" placeholder="URL photo" aria-label="url-photo" aria-labelledby="label-url-photo" maxlength="255" required>
 						</div>
 						<div class="btnCreate text-end">
 							<button type="submit" class="btn" value="create"><img src="/img/save.png" alt="save button"></button>
@@ -31,7 +31,6 @@
 			</div>
 		</div>
 	</div>
-</div>
 </div>
 <div class="container justify-content-center" style="max-width: 62rem">
 	<a class="btn mt-5 mb-1" href="{{ route('home') }}"><img src="/img/return.png" alt="return button"></a>
--- resources/views/editImage.blade.php
+++ resources/views/editImage.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.app')
 @section('content')
-	<div class="containerCreate">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-					<div class="card">
-						<div class="card-body">
-							<h2 class="cardTitle mb-4 d-flex flex-wrap row justify-content-center">Edit image</h2>
-								<form class="justify-content-center" action="{{ route('updateImage', $image->id) }}" method="post">
-                  @method('PATCH')
-                  @csrf
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="inputGroup-sizing-default">Title</span>
-									<input type="text" name="title" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{$image->title}}">
-								</div>
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="inputGroup-sizing-default">Year</span>
-									<input type="text" name="year" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{$image->year}}">
-								</div>
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="inputGroup-sizing-default">Description</span>
-									<input type="text" name="description" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"value="{{$image->description}}">
-								</div>
-								<div class="input-group mb-3">
-									<span class="input-group-text" id="inputGroup-sizing-default">Photo</span>
-									<input type="text" name="img" class="form-control uploadPhoto" aria-label="Sizing example input" aria-describedby="inputGoup-sizing-default" placeholder="{{$image->img}}">
-								</div>
-								<div class="btnCreate text-end">
-									<button type="submit" class="btn" value="create"><img src="/img/save.png" alt="save button"></button>
-								</div>
-							</form>
+<div class="containerEdit">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<h2 class="cardTitle mb-4 d-flex flex-wrap row justify-content-center">Edit Image</h2>
+					<form class="justify-content-center" action="{{ route('updateImage', $image->id) }}" method="post">
+						@method('PATCH')
+						@csrf
+						<div class="input-group mb-3">
+							<span class="input-group-text" id="label-title">Title</span>
+							<input type="text" name="title" class="form-control" aria-label="title" aria-describedby="label-title" value="{{$image->title}}" maxlength="150" required>
 						</div>
-					</div>
+						<div class="input-group mb-3">
+							<span class="input-group-text" id="label-year">Year</span>
+							<input type="text" name="year" class="form-control" aria-label="year" aria-describedby="label-year" value="{{$image->year}}" maxlength="4">
+						</div>
+						<div class="input-group mb-3">
+							<span class="input-group-text" id="label-description">Description</span>
+							<textarea name="description" rows="3" class="form-control" aria-label="description" aria-describedby="label-description" maxlength="500">{{$image->description}}</textarea>
+						</div>
+						<div class="input-group mb-3">
+							<span class="input-group-text" id="label-photo">Photo</span>
+							<input type="text" name="img" class="form-control" aria-label="photo" aria-describedby="label-photo" value="{{$image->img}}">
+						</div>
+						<div class="btnCreate text-end">
+							<button type="submit" class="btn" value="create"><img src="/img/save.png" alt="save button"></button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="container justify-content-center" style="max-width: 62rem">
-        <a class="btn mt-5 mb-1" href="{{ route('home') }}"><img src="/img/return.png" alt="button back to home view"></a>
-  </div>
+</div>
+<div class="container justify-content-center" style="max-width: 62rem">
+	<a class="btn mt-5 mb-1" href="{{ route('home') }}"><img src="/img/return.png" alt="button back to home view"></a>
+</div>
 @endsection
